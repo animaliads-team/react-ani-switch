0fcc22be7ce5d605634e15e9be0e55a2b78b994b
animaliads-team <ds.animalia@gmail.com>
2021-09-27 10:09:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ani-textfield-yfpug3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ani-textfield-yfpug3)
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@animaliads/css-tokens/global-tokens.css';
+import '@animaliads/animalia-brand/theme.css';
+import '@animaliads/ani-switch';
+import './style.css';
+
+export default function App() {
+  return <ani-switch></ani-switch>;
+}
